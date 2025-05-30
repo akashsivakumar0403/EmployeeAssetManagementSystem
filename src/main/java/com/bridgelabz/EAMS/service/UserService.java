38d974c22c241c7481a467fd334041755c13e17d
akashsivakumar0403 <akashsivakumar0403@gmail.com>
2025-05-31 01:18:54 +0530

    [Akash] added RoleManagement

--- src/main/java/com/bridgelabz/EAMS/service/UserService.java
+++ src/main/java/com/bridgelabz/EAMS/service/UserService.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.EAMS.service;
+import java.util.List;
+
 import com.bridgelabz.EAMS.dto.LoginRequest;
 import com.bridgelabz.EAMS.dto.RegistrationRequest;
 import com.bridgelabz.EAMS.dto.UserResponse;
+import com.bridgelabz.EAMS.entity.UserRole;
 
 public interface UserService {
     UserResponse registerUser(RegistrationRequest request);
     UserResponse loginUser(LoginRequest request);
+    List<UserResponse> getAllUsers();
+    UserResponse updateUserRole(Long id, UserRole newRole);
 }
