fa11040ff4a85b9fa24da2cef7ee38791d3196b7
akashsingh3009 <akash.ipg30@gmail.com>
2025-05-31 11:03:47 +0530

    [akash.k] added exceptions

---
+++ src/main/java/com/bridgelabz/EAMS/exception/AssetNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.EAMS.exception;
+
+public class AssetNotFoundException extends RuntimeException {
+    public AssetNotFoundException(String assetName) {
+        super("Asset not found: " + assetName);
+    }
+}
---
+++ src/main/java/com/bridgelabz/EAMS/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.EAMS.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String username) {
+        super("Username already exists: " + username);
+    }
+}
