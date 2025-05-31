db1acf2c15087a312814ba011b8539b4be874e56
Madhumitha A <137876422+madhu1703@users.noreply.github.com>
2025-05-31 09:53:10 +0530

    [madhumitha] added feature-alertsEmail

--- src/main/java/com/bridgelabz/EAMS/service/AlertService.java
+++ src/main/java/com/bridgelabz/EAMS/service/AlertService.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.EAMS.service;
 
-public class AlertService {
+import com.bridgelabz.EAMS.dto.SensorDataRequest;
+import com.bridgelabz.EAMS.dto.AlertResponse;
 
+import java.util.List;
+
+public interface AlertService {
+    void checkAndCreateAlert(SensorDataRequest request); // called from sensor ingestion
+    List<AlertResponse> getAllAlerts();
+    List<AlertResponse> getActiveAlerts();
+    void resolveAlert(Long alertId);
 }
